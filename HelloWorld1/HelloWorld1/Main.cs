0b8f8cc12e5ea7d62f860906a7c1ffe09a2c7a8e
TopA-Teacher <TopA-Teacher@DESKTOP-HJNBQEA>
2019-01-14 11:17:26 +0900

    WPF HelloWorld~

---
+++ HelloWorld1/HelloWorld1/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+namespace HelloWorld1
+{
+    class Main2
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run( new MainWindow() );
+        }
+    }
+}
