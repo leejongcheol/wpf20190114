d5e9b7e26dbb41046f2e4fa76ffac4514797a771
TopA-Teacher <TopA-Teacher@DESKTOP-HJNBQEA>
2019-01-15 14:41:50 +0900

    DataContext는 StackPanel에 한 번 설정되 었고 Label과 Button은 ScrollBar와 바인딩 되었다. Label에 바인딩 된 프로퍼티는 Content이지만 Button에 바인딩 된것은 FontSize이므로 ScrollBar를 움직이면 Button 내부의 텍스트가 점점 커지고 Button도 커진다.

---
+++ DataContextDataBinding/DataContextDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DataContextDataBinding
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClicked(object sender, RoutedEventArgs e)
+        {
+            Emp e1 = Grid1.DataContext as Emp;
+            System.Console.WriteLine(e1.Ename);
+            System.Console.WriteLine(e1.City);
+        }
+    }
+
+    public class Emp
+    {
+        public string Ename
+        {
+            get;
+            set;
+        }
+        public string City
+        {
+get;
+            set;
+        }
+    }
+}
