da643c828bb4ae918a50b95844e5e6cbb1371814
TopA-Teacher <TopA-Teacher@DESKTOP-HJNBQEA>
2019-01-15 10:54:38 +0900

    사용자 입력을 Window의 Content 속성에 매핑하기

---
+++ WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Text;
+namespace WpfApp4
+{
+    public partial class MainWindow : Window
+    {
+        string s = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title = "사용자 입력을 Window의 Content 속성에 매핑하기";
+            Content = s;
+        }
+
+        protected override void OnTextInput(TextCompositionEventArgs args)
+        {
+            string str = Content.ToString();
+            if (args.Text == "\b")
+            {
+                if (str.Length > 0)
+                {
+                    s = s.Substring(0, s.Length - 1);
+                }
+            }
+            else
+            {
+                s += args.Text;
+            }
+
+            Content = s;
+        }
+    }
+}
