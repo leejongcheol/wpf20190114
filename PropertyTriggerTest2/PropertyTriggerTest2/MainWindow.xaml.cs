a2b0d7009bc5599014adb3461fe1fdba27ccb1fe
TopA-Teacher <TopA-Teacher@DESKTOP-HJNBQEA>
2019-01-16 16:44:14 +0900

    WPF PropertyTrigger Test2

---
+++ PropertyTriggerTest2/PropertyTriggerTest2/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PropertyTriggerTest2
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            Thread.Sleep(1000);
+            Button btn = args.Source as Button;
+            MessageBox.Show(btn.Content + " has been clicked", Title);
+        }
+
+    }
+}
