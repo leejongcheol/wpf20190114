c2fcfb298a15b86b5146c438a55e7a2f310c0a49
TopA-Teacher <TopA-Teacher@DESKTOP-HJNBQEA>
2019-01-16 14:41:36 +0900

    WPF StatusBar Sample

---
+++ StatusBarTest/StatusBarTest/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace StatusBarTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void txtEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            int row = txtEditor.GetLineIndexFromCharacterIndex(txtEditor.CaretIndex);
+            int col = txtEditor.CaretIndex -
+           txtEditor.GetCharacterIndexFromLineIndex(row);
+            lblCursorPosition.Text = "Line " + (row + 1) + ", Char " + (col + 1);
+        }
+    }
+}
