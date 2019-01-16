07c3b381951bb76767b8ed8d3469fa50a3f03c64
TopA-Teacher <TopA-Teacher@DESKTOP-HJNBQEA>
2019-01-16 16:17:07 +0900

    WPF EventSetter Sample

---
+++ EventSetterTest/EventSetterTest/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EventSetterTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            Button btn = args.Source as Button;
+            MessageBox.Show($"{btn.Content}({btn.Name}) has been clicked {Title}");
+        }
+    }
+}
